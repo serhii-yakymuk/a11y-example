033ccecbbe9baba59ba3b0d76ef37f484450a2d5
Serhii Yakymuk <Serhii.Yakymuk@eleks.com>
2020-04-01 15:13:06 +0300

    Updates

---
+++ precache-manifest.523adfb7d643a98e578350f5cb9adf78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b2c950a87f741227991abca65bf914",
+    "url": "/a11y-example/index.html"
+  },
+  {
+    "revision": "b0288c12f5ee0def31fa",
+    "url": "/a11y-example/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "9a55681abcce94d8ce00",
+    "url": "/a11y-example/static/js/2.a7a56a64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/a11y-example/static/js/2.a7a56a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0288c12f5ee0def31fa",
+    "url": "/a11y-example/static/js/main.bbb0f812.chunk.js"
+  },
+  {
+    "revision": "7395d4f93376d9d7247c",
+    "url": "/a11y-example/static/js/runtime-main.14880753.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/a11y-example/static/media/logo.ee7cd8ed.svg"
+  }
+]);
