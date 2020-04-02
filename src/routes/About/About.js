5a330c3a7ca6642cc223292fc050098780f1d74f
Serhii Yakymuk <sergiy.yakymuk@gmail.com>
2020-04-02 17:53:10 +0300

    Add About info

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -6,6 +6,13 @@ const About = () => {
   return (
     <div>
       <h1>About Author</h1>
+      <p>
+        My name is Serhii, I'm working as a Front End Developer at ELEKS, Lviv.
+      </p>
+      <p>
+        Currently, I'm trying to expand my knowledge of accessible technologies in the Web. <br />
+        Feel free to contact me if you have any questions :)
+      </p>
     </div>
   );
 }
